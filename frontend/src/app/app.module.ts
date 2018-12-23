a599ab8b031c52f83dc3439f2c9ccc9f34653256
Dragashi <daniel.wehrmaker@gmx.de>
2018-12-23 16:22:02 +0100

    init frontend

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,55 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { RouterModule, Routes } from '@angular/router';
+import { DealersComponent } from './dealers/dealers.component';
+import { ProductsComponent } from './products/products.component';
+import { AppRoutingModule } from './/app-routing.module';
+
+const appRoutes: Routes = [
+  { path: 'overview', component: CalendarComponent },
+  { path: 'products', component: ProductsComponent },
+  {
+    path: 'dealers',
+    component: DealersComponent,
+  },
+  { path: '',
+    redirectTo: '/overview',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CalendarComponent,
+    PageNotFoundComponent,
+    DealersComponent,
+    ProductsComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    CalendarModule.forRoot({
+      provide: DateAdapter,
+      useFactory: adapterFactory
+    }),
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    // other imports here
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
