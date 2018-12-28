79d30d1328e5cec8e868912a62cfcfd6b9715992
Dragashi <daniel.wehrmaker@gmx.de>
2018-12-28 15:02:47 +0100

    product view

--- frontend/src/app/products/products.component.ts
+++ frontend/src/app/products/products.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ProductsService } from './products.service';
+import { Dealer } from '../dealers/dealer.service';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent implements OnInit {
-
-  constructor() { }
-
+  public products = [];
+  public dealerProduct: String = '';
+  public dealers: Dealer[] = [];
+  constructor(private productService: ProductsService) { }
   ngOnInit() {
+    this.getProducts();
   }
-
+  getProducts(): void {
+    this.productService.getProducts().subscribe(products => this.products = products);
+}
+  showDealersForProducts(productName: String): void  {
+    console.log(productName);
+    this.dealerProduct = productName;
+    this.dealers = [{
+    'moodle_id': 2,
+    'first_name': 'cornelius',
+    'last_name': 'black',
+    'mobile': '010123213',
+    'email2': 'black@black',
+    'available': true,
+    'room': '108',
+    'blocked': false,
+    'away': true,
+    'away_reason': 'no bock'},
+    {
+      'moodle_id': 2,
+      'first_name': 'Albus',
+      'last_name': 'Dumbledore',
+      'mobile': '010123213',
+      'email2': 'black@black',
+      'available': true,
+      'room': '108',
+      'blocked': false,
+      'away': true,
+      'away_reason': 'no bock'}];
+  }
 }
