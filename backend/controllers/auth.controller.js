2ecab0c7f329e97cbc7433e36c5662a16c07cf60
Sh4d0w <Patrick-DE@users.noreply.github.com>
2019-01-05 23:40:56 +0100

    repaired auth controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -53,8 +53,8 @@ exports.user_logout = function (req, res) {
 
 exports.user_detail = function (req, res, next) {
 	User.findById(req.userId, function (err, user) {
-		if (err) return res.status(500).send({"There was a problem finding the user."});
-		if (!user) return res.status(404).send({"No user found."});
+		if (err) return res.status(500).send({err: "There was a problem finding the user."});
+		if (!user) return res.status(404).send({err: "No user found."});
 		res.status(200).send(user);
 	});
 };
