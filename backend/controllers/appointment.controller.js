c40f074eff50de6524463d220657f6e60235f439
David Nadoba <dnadoba@gmail.com>
2019-01-06 19:13:17 +0100

    fix get appointment by user_id

--- backend/controllers/appointment.controller.js
+++ backend/controllers/appointment.controller.js
@@ -3,7 +3,7 @@ var Appointment = require('../models/appointment.model');
 // Display list of all appointments.
 exports.appointment_list = function(req, res) {
     Appointment
-      .findById(req.userId)
+      .find({user_id: req.userId})
       .populate('block_id')
       .exec(function (err, appointments) {
         if (err) return res.send(err.errmsg);
