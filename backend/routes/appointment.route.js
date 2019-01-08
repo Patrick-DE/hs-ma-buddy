7970a2be806665b03cce82c552228aa6fe02f991
David Nadoba <dnadoba@gmail.com>
2019-01-09 00:14:06 +0100

    fix appointment creation

--- backend/routes/appointment.route.js
+++ backend/routes/appointment.route.js
@@ -3,21 +3,20 @@ var router = express.Router();
 const verifyToken = require('../verifyToken')
 
 var appointment_controller = require('../controllers/appointment.controller');
-var VerifyToken = require('../verifyToken');
 
 //Get all appointment
 router.get('/', verifyToken, appointment_controller.appointment_list);
 
 //Get appointment details
-router.get('/:id', VerifyToken, appointment_controller.appointment_detail);
+router.get('/:id', verifyToken, appointment_controller.appointment_detail);
 
 //Create a new appointment
-router.post('/', VerifyToken, appointment_controller.appointment_create);
+router.post('/', verifyToken, appointment_controller.appointment_create);
 
 //Update a appointment
-router.delete('/:id', VerifyToken, appointment_controller.appointment_delete);
+router.delete('/:id', verifyToken, appointment_controller.appointment_delete);
 
 //Delete a appointment
-router.put('/:id', VerifyToken, appointment_controller.appointment_update);
+router.put('/:id', verifyToken, appointment_controller.appointment_update);
 
 module.exports = router;
